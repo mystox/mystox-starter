3a2cf3435e7383a5404c3fb2ac957b9b26f20dde
fengw <fengw@kongtrolink.com>
2019-04-23 16:00:12 +0800

    1.新增处理铁塔平台设置数据点功能 2.新增处理铁塔平台设置门限值功能

---
+++ yyds-minifsu-project/minifsu-tower/src/main/java/com/kongtrolink/framework/entity/xml/msg/SetThresholdAck.java
@@ -0,0 +1,67 @@
+package com.kongtrolink.framework.entity.xml.msg;
+
+import com.kongtrolink.framework.entity.CntbPktTypeTable;
+import com.kongtrolink.framework.entity.xml.base.Info;
+import com.kongtrolink.framework.entity.xml.base.PKType;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "Info")
+public class SetThresholdAck extends Info {
+
+    @XmlElement(name = "FsuId")
+    private String fsuId;
+    @XmlElement(name = "FsuCode")
+    private String fsuCode;
+    @XmlElement(name = "Result")
+    private int result;
+    @XmlElement(name = "DeviceList")
+    private List<Device> deviceList;
+
+    public SetThresholdAck() {
+        deviceList = new ArrayList<>();
+    }
+
+    @Override
+    public PKType pkType() {
+        return new PKType(CntbPktTypeTable.SET_POINT_ACK, CntbPktTypeTable.SET_POINT_ACK_CODE);
+    }
+
+    public String getFsuId() {
+        return fsuId;
+    }
+
+    public void setFsuId(String fsuId) {
+        this.fsuId = fsuId;
+    }
+
+    public String getFsuCode() {
+        return fsuCode;
+    }
+
+    public void setFsuCode(String fsuCode) {
+        this.fsuCode = fsuCode;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    public List<Device> getDeviceList() {
+        return deviceList;
+    }
+
+    public void setDeviceList(List<Device> deviceList) {
+        this.deviceList = deviceList;
+    }
+}
