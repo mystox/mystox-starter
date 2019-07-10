586f67245900abc8b11202c33adb25ef4e0cdf51
fengw <fengw@kongtrolink.com>
2019-07-10 17:21:37 +0800

    [add]新增redis加锁解锁方法

--- yyds-core/src/main/java/com/kongtrolink/framework/core/utils/RedisUtils.java
+++ yyds-core/src/main/java/com/kongtrolink/framework/core/utils/RedisUtils.java
@@ -645,4 +645,47 @@ public class RedisUtils
 
         return (List<T>) list;
     }
+
+    /**
+     * 加锁
+     * @param key 需要加锁的id
+     * @param timeout 当前线程操作时的超时时间，单位毫秒，小于0则无超时时间
+     *                这个地方不需要去设置redis的expire，也不需要超时后手动去删除该key，
+     *                因为会存在并发的线程都会去删除，造成上一个锁失效，结果都获得锁去执行，并发操作失败了就。
+     * @return 如果取到锁返回值，未取到锁返回null
+     */
+    public String lock(String key, int timeout) {
+
+        int increment = timeout < 0 ? 0 : timeout;
+        String value = System.currentTimeMillis() + increment + "";
+
+        //如果key值不存在，则返回 true，且设置 value
+        if (redisTemplate.opsForValue().setIfAbsent(key, value)) {
+            return value;
+        }
+
+        //获取key的值，判断是是否超时
+        Object curVal = redisTemplate.opsForValue().get(key);
+        if (curVal != null && (timeout >= 0 && Long.parseLong(curVal.toString()) < System.currentTimeMillis())) {
+            //获得之前的key值，同时设置当前的传入的value。这个地方可能几个线程同时过来，但是redis本身天然是单线程的，所以getAndSet方法还是会安全执行，
+            //首先执行的线程，此时curVal当然和oldVal值相等，因为就是同一个值，之后该线程set了自己的value，后面的线程就取不到锁了
+            String oldVal = redisTemplate.opsForValue().getAndSet(key, value).toString();
+            if(oldVal != null && oldVal.equals(curVal)) {
+                return value;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 解锁
+     * @param key
+     * @param value
+     */
+    public void unlock(String key, String value) {
+        Object curVal = redisTemplate.opsForValue().get(key);
+        if (curVal != null && curVal.toString().equals(value)) {
+            redisTemplate.opsForValue().getOperations().delete(key);
+        }
+    }
 }
