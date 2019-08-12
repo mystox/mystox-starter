748ab391f3a79cf122142aae3075242793e62c88
mystox <mystox@163.com>
2019-08-12 11:29:23 +0800

    [add] mqtt消息模块

---
+++ yyds-mqtt/yyds-mqtt-starter/src/main/java/tech/mystox/demo/config/MqttConfig.java
@@ -0,0 +1,147 @@
+package tech.mystox.demo.config;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+
+/**
+ * Created by mystoxlol on 2019/8/5, 14:35.
+ * company: kongtrolink
+ * description:
+ * update record:
+ */
+@Configuration
+public class MqttConfig {
+
+    /**
+     * 订阅的bean名称
+     */
+    public static final String CHANNEL_NAME_IN = "mqttInboundChannel112313123123";
+    /**
+     * 发布的bean名称
+     */
+    public static final String CHANNEL_NAME_OUT = "mqttOutboundChannel1123213123123";
+
+
+    /**
+     * MQTT连接器选项
+     *
+     * @return {@link org.eclipse.paho.client.mqttv3.MqttConnectOptions}
+     */
+    @Bean
+    public MqttConnectOptions getMqttConnectOptions() {
+        MqttConnectOptions options = new MqttConnectOptions();
+        // 设置是否清空session,这里如果设置为false表示服务器会保留客户端的连接记录，
+        // 这里设置为true表示每次连接到服务器都以新的身份连接
+        options.setCleanSession(true);
+        // 设置连接的用户名
+//        options.setUserName(username);
+        // 设置连接的密码
+//        options.setPassword(password.toCharArray());
+        options.setServerURIs(StringUtils.split("tcp://172.16.5.26:1883", ","));
+        // 设置超时时间 单位为秒
+        options.setConnectionTimeout(10);
+        // 设置会话心跳时间 单位为秒 服务器会每隔1.5*20秒的时间向客户端发送心跳判断客户端是否在线，但这个方法并没有重连的机制
+        options.setKeepAliveInterval(20);
+        // 设置“遗嘱”消息的话题，若客户端与服务器之间的连接意外中断，服务器将发布客户端的“遗嘱”消息。
+        options.setWill("willTopic", "offOnline".getBytes(), 1, false);
+        return options;
+    }
+
+    /**
+     * MQTT客户端
+     *
+     * @return {@link org.springframework.integration.mqtt.core.MqttPahoClientFactory}
+     */
+    @Bean
+    public MqttPahoClientFactory mqttClientFactory() {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        factory.setConnectionOptions(getMqttConnectOptions());
+        return factory;
+    }
+
+    /**
+     * MQTT信息通道（生产者）
+     *
+     * @return {@link org.springframework.messaging.MessageChannel}
+     */
+    @Bean(name = CHANNEL_NAME_OUT)
+    public MessageChannel mqttOutboundChannel() {
+        return new DirectChannel();
+    }
+
+    /**
+     * MQTT消息处理器（生产者）
+     *
+     * @return {@link org.springframework.messaging.MessageHandler}
+     */
+    @Bean
+    @ServiceActivator(inputChannel = CHANNEL_NAME_OUT)
+    public MessageHandler mqttOutbound() {
+        MqttPahoMessageHandler messageHandler = new MqttPahoMessageHandler(
+                "1231421312314213",
+                mqttClientFactory());
+        messageHandler.setAsync(true);
+        messageHandler.setDefaultTopic("topic1");
+        return messageHandler;
+    }
+
+    /**
+     * MQTT消息订阅绑定（消费者）
+     *
+     * @return {@link org.springframework.integration.core.MessageProducer}
+     */
+    @Bean
+    public MessageProducer inbound() {
+        // 可以同时消费（订阅）多个Topic
+        MqttPahoMessageDrivenChannelAdapter adapter =
+                new MqttPahoMessageDrivenChannelAdapter(
+                        "sdfafefsdfefdfwafdsaf", mqttClientFactory(),
+                        StringUtils.split("topic1", ","));
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        // 设置订阅通道
+        adapter.setOutputChannel(mqttInboundChannel());
+        return adapter;
+    }
+
+    /**
+     * MQTT信息通道（消费者）
+     *
+     * @return {@link org.springframework.messaging.MessageChannel}
+     */
+    @Bean(name = CHANNEL_NAME_IN)
+    public MessageChannel mqttInboundChannel() {
+        return new DirectChannel();
+    }
+
+
+    /**
+     * MQTT消息处理器（消费者）
+     *
+     * @return {@link org.springframework.messaging.MessageHandler}
+     */
+    @Bean
+    @ServiceActivator(inputChannel = CHANNEL_NAME_IN)
+    public MessageHandler handler() {
+        return message -> {
+//                LOGGER.error("===================={}============", message.getPayload());
+            System.out.println("===================={}============"+message.getPayload());
+        };
+    }
+
+
+
+}
