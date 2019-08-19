1c463130d7a1e6eb7fb6dafeddc8e20f6d3d4017
liudongdong <liudd@kongtrolink.com>
2019-08-19 13:17:15 +0800

    DI功能，实时数据和DI点都发送给铁塔

--- yyds-minifsu-project/minifsu-monitor/src/main/java/com/kongtrolink/framework/execute/module/ExecuteModule.java
+++ yyds-minifsu-project/minifsu-monitor/src/main/java/com/kongtrolink/framework/execute/module/ExecuteModule.java
@@ -194,12 +194,8 @@ public class ExecuteModule extends RpcNotifyImpl implements ModuleInterface {
         timeDateService.updateData(fsu, dev_colId_valMap);
         if(DI_dev_colId_valMap.size() != 0){
             //拼接新的fsu，并转换成消息体发给控制中心
-            JsonFsu DIFsu = new JsonFsu();
-            DIFsu.setDtm(fsu.getDtm());
-            DIFsu.setPktType(fsu.getPktType());
-            DIFsu.setSN(fsu.getSN());
-            timeDateService.jointFsu(DIFsu, DI_dev_colId_valMap);
-            JSONObject fsuJsonObj = (JSONObject)JSON.toJSON(DIFsu);
+            timeDateService.jointFsu(fsu, DI_dev_colId_valMap);
+            JSONObject fsuJsonObj = (JSONObject)JSON.toJSON(fsu);
             moduleMsg.setPayload(fsuJsonObj);
             //将变化DI数据发送给铁塔
             registerData(msgId, moduleMsg);
