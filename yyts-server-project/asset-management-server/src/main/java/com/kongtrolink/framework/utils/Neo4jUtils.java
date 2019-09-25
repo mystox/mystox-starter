5451924f558d5806c2d33419636d76eee88c7a20
fengw <fengw@kongtrolink.com>
2019-09-25 09:06:27 +0800

    [change]新增CI信息增删改，新增CI信息关联关系增删

--- yyts-server-project/asset-management-server/src/main/java/com/kongtrolink/framework/utils/Neo4jUtils.java
+++ yyts-server-project/asset-management-server/src/main/java/com/kongtrolink/framework/utils/Neo4jUtils.java
@@ -27,4 +27,46 @@ public class Neo4jUtils {
         result.add(Integer.valueOf(codes[1]));
         return result;
     }
+
+    /**
+     * 获取CI类型组合编码
+     * @param code1 第一级类型编码
+     * @param code2 第二级类型编码
+     * @param code3 第三级类型编码
+     * @return 组合编码
+     */
+    public static String getCITypeCode(String code1, String code2, String code3) {
+        return code1 + "-" + code2 + "-" + code3;
+    }
+
+    /**
+     * 获取CI类型编码
+     * @param id CI ID
+     * @return CI类型编码
+     */
+    public static String getTypeCode(String id) {
+        return id.split("-")[2];
+    }
+
+    /**
+     * 获取业务编码
+     * @param id CI ID
+     * @return 业务编码
+     */
+    public static String getBusinessCode(String id) {
+        String[] contents = id.split("-");
+        return contents[3] + "-" + contents[4];
+    }
+
+    /**
+     * 获取CIID
+     * @param ciTypeCode CI的类型编码，从1级到3级，通过‘-’分隔
+     * @param sn ci的SN
+     * @param enterpriseCode 企业编码
+     * @param serverCode 服务编码
+     * @return CI的Id
+     */
+    public static String getCIId(String ciTypeCode, String sn, int enterpriseCode, int serverCode) {
+        return ciTypeCode + "-" + enterpriseCode + "-" + serverCode + "-" + sn;
+    }
 }
