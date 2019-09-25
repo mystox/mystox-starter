4994ad092fdd69a80f50bdc876a3dd91e6082149
mystox <mystox@163.com>
2019-09-25 17:23:49 +0800

    [add] 增加web session共享功能

---
+++ yyds-core/src/main/java/com/kongtrolink/framework/core/entity/session/Const.java
@@ -0,0 +1,19 @@
+/**
+ * *****************************************************
+ * Copyright (C) Kongtrolink techology Co.ltd - All Rights Reserved
+ *
+ * This file is part of Kongtrolink techology Co.Ltd property.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ ******************************************************
+ */
+package com.kongtrolink.framework.core.entity.session;
+
+public class Const {
+    
+    public static final String SESSION_USER = "SESSION_USER";
+    public static final String SESSION_CURRENT_STAMP = "SESSION_CURRENT_STAMP";
+    public static final String SESSION_LAST_REQUEST_TIME = "SESSION_LAST_REQUEST_TIME";
+    
+}
