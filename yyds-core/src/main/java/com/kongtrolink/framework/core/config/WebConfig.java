aa59e8a5f2b1c25ac787bc30f517c248bbe6bbc0
mystox <mystox@163.com>
2019-09-27 13:24:24 +0800

    [change] session的部分修改

--- yyds-core/src/main/java/com/kongtrolink/framework/core/config/WebConfig.java
+++ yyds-core/src/main/java/com/kongtrolink/framework/core/config/WebConfig.java
@@ -1,6 +1,6 @@
 package com.kongtrolink.framework.core.config;
 
-import com.kongtrolink.framework.interceptor.SessionTimeoutInterceptor;
+import com.kongtrolink.framework.core.interceptor.SessionTimeoutInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ResourceUtils;
