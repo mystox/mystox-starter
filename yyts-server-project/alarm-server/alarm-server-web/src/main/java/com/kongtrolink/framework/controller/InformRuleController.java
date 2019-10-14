20a2ee3af8d0e38748891467d067a04a82ef33e6
mystox <mystox@163.com>
2019-10-14 16:13:48 +0800

    Merge branch 'mqtt-module' of http://git.kongtrolink.com:3000/mystox/YYDS into mqtt-module
    # Conflicts:
    #	yyds-common/src/main/java/com/kongtrolink/framework/entity/ServerName.java
    #	yyds-core/yyds-core.iml

--- yyts-server-project/alarm-server/alarm-server-web/src/main/java/com/kongtrolink/framework/controller/InformRuleController.java
+++ yyts-server-project/alarm-server/alarm-server-web/src/main/java/com/kongtrolink/framework/controller/InformRuleController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Date;
