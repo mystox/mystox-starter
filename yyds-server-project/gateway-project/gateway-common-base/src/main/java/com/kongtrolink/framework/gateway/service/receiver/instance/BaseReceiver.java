79e10dafc13c039a990942c732718eb7edc95ff8
mystox <mystox@163.com>
2019-10-28 15:16:31 +0800

    [change] 网关mqtt阻塞问题

--- yyds-server-project/gateway-project/gateway-common-base/src/main/java/com/kongtrolink/framework/gateway/service/receiver/instance/BaseReceiver.java
+++ yyds-server-project/gateway-project/gateway-common-base/src/main/java/com/kongtrolink/framework/gateway/service/receiver/instance/BaseReceiver.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
 import static com.kongtrolink.framework.gateway.mqtt.GatewayMqttConfig.CHANNEL_NAME_IN;
@@ -30,26 +31,32 @@ public class BaseReceiver extends ReceiveHandler {
     @Autowired
     ParseService parseService;
 
+    @Autowired
+    ThreadPoolTaskExecutor mqttExecutor;
+
     @ServiceActivator(inputChannel = CHANNEL_NAME_IN/*, outputChannel = CHANNEL_NAME_OUT*/)
     public void messageReceiver(Message<String> message) {
-        try{
-            //至少送达一次存在重复发送的几率，所以订阅服务需要判断消息订阅的幂等性,幂等性可以通过消息属性判断是否重复发送
-            Boolean mqtt_duplicate = (Boolean) message.getHeaders().get("mqtt_duplicate");
-            if (mqtt_duplicate) {
-                logger.warn("message receive  duplicate [{}]", message);
+        mqttExecutor.execute(() -> {
+            try {
+                //至少送达一次存在重复发送的几率，所以订阅服务需要判断消息订阅的幂等性,幂等性可以通过消息属性判断是否重复发送
+                Boolean mqtt_duplicate = (Boolean) message.getHeaders().get("mqtt_duplicate");
+                if (mqtt_duplicate) {
+                    logger.warn("message receive  duplicate [{}]", message);
+                }
+                String topics = message.getHeaders().get("mqtt_topic").toString();
+                String payload = message.getPayload();
+                logger.info("[MQTT] Message arrived. topic: {} message: {}", topics, payload);
+                //iaiot/edge/yy/+/packetName/version
+                String sn = topics.split("/")[3];
+                String packetName = topics.split("/")[4];
+                String uuid = topics;
+                ParseProtocol parseProtocol = new ParseProtocol(packetName, sn, uuid, payload);
+                parseService.execute(JSONObject.toJSONString(parseProtocol));
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            String topics = message.getHeaders().get("mqtt_topic").toString();
-            String payload = message.getPayload();
-            logger.info("[MQTT] Message arrived. topic: {} message: {}", topics, payload);
-            //iaiot/edge/yy/+/packetName/version
-            String sn = topics.split("/")[3];
-            String packetName = topics.split("/")[4];
-            String uuid  = topics;
-            ParseProtocol parseProtocol = new ParseProtocol(packetName,sn,uuid,payload);
-            parseService.execute(JSONObject.toJSONString(parseProtocol));
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+
+        });
     }
 
 
@@ -67,8 +74,6 @@ public class BaseReceiver extends ReceiveHandler {
     public void restart() {
 
     }
-
-
 
 
 }
