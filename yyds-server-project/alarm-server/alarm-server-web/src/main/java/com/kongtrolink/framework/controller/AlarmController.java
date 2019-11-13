a45697139abec958e3bc61657b2979eb9ea64638
liudongdong <liudd@kongtrolink.com>
2019-11-13 15:34:09 +0800

    前端分页初步

--- yyds-server-project/alarm-server/alarm-server-web/src/main/java/com/kongtrolink/framework/controller/AlarmController.java
+++ yyds-server-project/alarm-server/alarm-server-web/src/main/java/com/kongtrolink/framework/controller/AlarmController.java
@@ -1,6 +1,7 @@
 package com.kongtrolink.framework.controller;
 
 import com.kongtrolink.framework.base.Contant;
+import com.kongtrolink.framework.base.DateUtil;
 import com.kongtrolink.framework.base.MongTable;
 import com.kongtrolink.framework.core.entity.session.BaseController;
 import com.kongtrolink.framework.entity.JsonResult;
@@ -17,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -40,18 +43,33 @@ public class AlarmController extends BaseController {
         String serverCode = alarmQuery.getServerCode();
         String type = alarmQuery.getType();
         String table = MongTable.ALARM_CURRENT;
-        if(Contant.HIST_ALARM.equals(type)) {
-            table = enterpriseCode + Contant.UNDERLINE + serverCode + Contant.UNDERLINE + MongTable.ALARM_HISTORY;
+        ListResult<DBObject> listResult = null;
+        if(Contant.CURR_ALARM.equals(type)){
+            List<DBObject> list= alarmService.list(alarmQuery, table);
+            int count = alarmService.count(alarmQuery, table);
+            listResult = new ListResult<>(list, count);
+        }else{
+
+            listResult = getHistoryAlarmList(alarmQuery);
         }
-        List<DBObject> list = alarmService.list(alarmQuery, table);
-        int count = alarmService.count(alarmQuery, table);
-        ListResult<DBObject> listResult = new ListResult<>(list, count);
+
         JsonResult jsonResult = new JsonResult(listResult);
         Auxilary auxilary = auxilaryService.getByEnterServerCode(enterpriseCode, serverCode);
         jsonResult.setOtherInfo(auxilary);
         return jsonResult;
     }
 
+    //历史告警多张表分页查询
+    private  ListResult<DBObject> getHistoryAlarmList(AlarmQuery alarmQuery){
+        //1，确定时间。如果开始接结束都没有选择，开始结束为今天到一个月前的今天
+        Date startBeginTime = alarmQuery.getStartBeginTime();
+        Date startEndTime = alarmQuery.getStartEndTime();
+        Calendar calendar = DateUtil.calendar;
+        if(null == startBeginTime && null == startEndTime){
+            startEndTime = new Date();
 
+        }
+        return null;
+    }
 
 }
