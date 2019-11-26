e00e79910611939b0ce14c96cd90369486c1fe2b
fengw <fengw@kongtrolink.com>
2019-11-26 09:18:43 +0800

    [add]添加测试功能

--- yyds-server-project/asset-management-server/src/main/java/com/kongtrolink/framework/controller/TestController.java
+++ yyds-server-project/asset-management-server/src/main/java/com/kongtrolink/framework/controller/TestController.java
@@ -22,6 +22,97 @@ public class TestController {
     @Resource(name = "assetManagementExecutor")
     ThreadPoolTaskExecutor taskExecutor;
 
+    @RequestMapping("/create")
+    public String testCreateCI(@RequestBody JSONObject requestBody) {
+
+        final String enterpriseCode = requestBody.getString("enterpriseCode");
+        final String serverCode = requestBody.getString("serverCode");
+        final int start = requestBody.getInteger("start");
+        final int end = requestBody.getInteger("end");
+
+        JSONObject jsonObjectFsu = new JSONObject();
+        jsonObjectFsu.put("enterpriseCode", enterpriseCode);
+        jsonObjectFsu.put("serverCode", serverCode);
+        jsonObjectFsu.put("address", "000000");
+        jsonObjectFsu.put("type", "yy38");
+        jsonObjectFsu.put("sn", "000000");
+        jsonObjectFsu.put("model", "fsu");
+        jsonObjectFsu.put("status", true);
+        jsonObjectFsu.put("user", "test");
+        jsonObjectFsu.put("remarks", "测试");
+        jsonObjectFsu.put("resourceNo", 1);
+        jsonObjectFsu.put("versionMajor", 1);
+        jsonObjectFsu.put("versionMinor", 1);
+        jsonObjectFsu.put("versionRevision", 2);
+
+        JSONObject jsonObjectPW = new JSONObject();
+        jsonObjectPW.put("enterpriseCode", enterpriseCode);
+        jsonObjectPW.put("serverCode", serverCode);
+        jsonObjectPW.put("address", "000000");
+        jsonObjectPW.put("type", "yy6");
+        jsonObjectPW.put("sn", "000000");
+        jsonObjectPW.put("model", "power");
+        jsonObjectPW.put("status", true);
+        jsonObjectPW.put("user", "test");
+        jsonObjectPW.put("remarks", "测试");
+        jsonObjectPW.put("resourceNo", 1);
+        jsonObjectPW.put("versionMajor", 1);
+        jsonObjectPW.put("versionMinor", 0);
+        jsonObjectPW.put("versionRevision", 5);
+
+        JSONObject jsonObjectENV = new JSONObject();
+        jsonObjectENV.put("enterpriseCode", enterpriseCode);
+        jsonObjectENV.put("serverCode", serverCode);
+        jsonObjectENV.put("address", "000000");
+        jsonObjectENV.put("type", "yy18");
+        jsonObjectENV.put("sn", "000000");
+        jsonObjectENV.put("model", "environment");
+        jsonObjectENV.put("status", true);
+        jsonObjectENV.put("user", "test");
+        jsonObjectENV.put("remarks", "测试");
+        jsonObjectENV.put("resourceNo", 1);
+        jsonObjectENV.put("versionMajor", 1);
+        jsonObjectENV.put("versionMinor", 2);
+        jsonObjectENV.put("versionRevision", 9);
+
+        neo4jDBService.searchCIConnectionType();
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        Date now = new Date();
+        System.out.println(simpleDateFormat.format(now) + ":Start");
+
+        for (int i = start; i <= end; ++i) {
+            String sn = "00000" + i;
+            sn = sn.substring(sn.length() - 5);
+            jsonObjectFsu.put("sn", "438" + sn);
+            jsonObjectPW.put("sn", "406" + sn);
+            jsonObjectENV.put("sn", "418" + sn);
+
+            String fsuId = neo4jDBService.addCI(jsonObjectFsu);
+            String powerId = neo4jDBService.addCI(jsonObjectPW);
+            String envId = neo4jDBService.addCI(jsonObjectENV);
+
+            JSONObject relationship = new JSONObject();
+
+            relationship.put("id1", fsuId);
+            relationship.put("id2", powerId);
+            relationship.put("type", "Physical");
+            neo4jDBService.addCIRelationship(relationship);
+
+            relationship.put("id2", envId);
+            neo4jDBService.addCIRelationship(relationship);
+
+            if (i % 1000 == 0) {
+                now = new Date();
+                System.out.println(simpleDateFormat.format(now) + ":" + i);
+            }
+        }
+
+        now = new Date();
+        System.out.println(simpleDateFormat.format(now) + ":" + end);
+        return "1";
+    }
+
     @RequestMapping("/search")
     public String testSearchCI(@RequestBody JSONObject requestBody) {
 
@@ -30,6 +121,7 @@ public class TestController {
         final String serverCode = requestBody.getString("serverCode");
         final int times = requestBody.getInteger("times");
         final int pageNum = requestBody.getInteger("pageNum");
+        final int total = requestBody.getInteger("total");
 
         Boolean[] threadBoolean = new Boolean[threadCount];
 
@@ -38,15 +130,18 @@ public class TestController {
         for (int i = 0; i < threadCount; ++i) {
             threadBoolean[i] = false;
             final int index = i;
-            taskExecutor.execute(()->searchCI(index, enterpriseCode, serverCode, times, pageNum));
+            taskExecutor.execute(()->searchCI(index, enterpriseCode, serverCode, times, total, pageNum));
         }
 
         return "1";
     }
 
-    private void searchCI(int index, String enterpriseCode, String serverCode, int times, int pageNum) {
+    private void searchCI(int index, String enterpriseCode, String serverCode, int times, int total, int pageNum) {
 
-        int pageTotal = 60000 / pageNum;
+        int pageTotal = total / pageNum;
+        if ((total % pageNum) > 0) {
+            pageTotal++;
+        }
 
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("enterpriseCode", enterpriseCode);
