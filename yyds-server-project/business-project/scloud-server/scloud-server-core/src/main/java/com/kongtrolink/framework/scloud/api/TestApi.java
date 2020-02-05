87136db144f46ebad468aabbea1f45dfbd67cda5
mystox <mystox@163.com>
2020-02-05 15:47:40 +0800

    [add] cloud-core 目录结构的调整

---
+++ yyds-server-project/business-project/scloud-server/scloud-server-core/src/main/java/com/kongtrolink/framework/scloud/api/TestApi.java
@@ -0,0 +1,10 @@
+package com.kongtrolink.framework.scloud.api;
+
+/**
+ * \* @Author: mystox
+ * \* Date: 2020/2/5 15:45
+ * \* Description:
+ * \
+ */
+public class TestApi {
+}
---
+++ yyds-server-project/business-project/scloud-server/scloud-server-core/src/main/java/com/kongtrolink/framework/scloud/core/TestCore.java
@@ -0,0 +1,10 @@
+package com.kongtrolink.framework.scloud.core;
+
+/**
+ * \* @Author: mystox
+ * \* Date: 2020/2/5 15:46
+ * \* Description:
+ * \
+ */
+public class TestCore {
+}
