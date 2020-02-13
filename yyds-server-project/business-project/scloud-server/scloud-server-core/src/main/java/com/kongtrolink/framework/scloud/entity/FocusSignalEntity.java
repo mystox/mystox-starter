3a286c53c6f22c68dfef126b54c947abaf7be3df
mag <Administrator>
2020-02-13 14:23:20 +0800

    scloud 实时数关注信号点

--- yyds-server-project/business-project/scloud-server/scloud-server-core/src/main/java/com/kongtrolink/framework/scloud/entity/FocusSignalEntity.java
+++ yyds-server-project/business-project/scloud-server/scloud-server-core/src/main/java/com/kongtrolink/framework/scloud/entity/FocusSignalEntity.java
@@ -18,10 +18,15 @@ public class FocusSignalEntity implements Serializable {
     private Integer id; //主键
     private String uniqueCode;
     private int deviceId;
-    //为了方便获取该关注点的数据 保存下面3个点
+    private String deviceType; //设备类型
     private String fsuCode; //FSU code
     private String deviceCode; //设备code
     private String cntbId;//信号灯的cntbId
+    private String userId; //用户ID
+    private String typeName;  //信号点名称
+    private String measurement; //单位
+    private String type;    //信号点类型。"0"-遥信信号(DI),"1"-遥测信号(AI),"2"-遥控信号(DO),"3"-遥调信号(AO)
+    private String alarmLevel;//告警级别
 
     public String getUniqueCode() {
         return uniqueCode;
@@ -69,5 +74,53 @@ public class FocusSignalEntity implements Serializable {
 
     public void setDeviceCode(String deviceCode) {
         this.deviceCode = deviceCode;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getMeasurement() {
+        return measurement;
+    }
+
+    public void setMeasurement(String measurement) {
+        this.measurement = measurement;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAlarmLevel() {
+        return alarmLevel;
+    }
+
+    public void setAlarmLevel(String alarmLevel) {
+        this.alarmLevel = alarmLevel;
     }
 }
