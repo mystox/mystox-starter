dea1141ae41f3e8a3e2921202d147ea485b88c67
mystox <mystox@163.com>
2020-03-09 15:31:45 +0800

    [add] 报表的对外接口

---
+++ yyds-server-project/reports-server/src/main/java/com/kongtrolink/framework/reports/entity/ReportsOpera.java
@@ -0,0 +1,12 @@
+package com.kongtrolink.framework.reports.entity;
+
+/**
+ * \* @Author: mystox
+ * \* Date: 2020/3/9 15:30
+ * \* Description:
+ * \
+ */
+public class ReportsOpera {
+
+    public final static String GETCI_SCLOUD = "getCISCloud"; //服务注册登录
+}
