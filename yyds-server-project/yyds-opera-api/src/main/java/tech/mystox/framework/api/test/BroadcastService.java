9ba693ea1b3bde1691baa2b9fd1fc471c672af99
mystox <mystox@163.com>
2020-08-27 16:36:01 +0800

    [add] 添加异步函数及其广播函数

---
+++ yyds-server-project/yyds-opera-api/src/main/java/tech/mystox/framework/api/test/BroadcastService.java
@@ -0,0 +1,16 @@
+package tech.mystox.framework.api.test;
+
+import tech.mystox.framework.stereotype.Opera;
+import tech.mystox.framework.stereotype.OperaCode;
+import tech.mystox.framework.stereotype.Register;
+
+import java.util.List;
+
+@Register
+public interface BroadcastService {
+
+    @OperaCode
+    public void callHelloWorld(String name, List<String> home);
+
+
+}
