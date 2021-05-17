2ecfd0b27a826a1178aed184924999c8cfe01287
mystox <mystox@163.com>
2021-05-17 18:16:24 +0800

    [add] 消息框架增加extension的注册

--- yyds-common/src/main/java/tech/mystox/framework/entity/ServerMsg.java
+++ yyds-common/src/main/java/tech/mystox/framework/entity/ServerMsg.java
@@ -1,5 +1,7 @@
 package tech.mystox.framework.entity;
 
+import java.util.Map;
+
 /**
  * Created by mystoxlol on 2019/9/30, 8:31.
  * company: mystox
@@ -7,7 +9,6 @@ package tech.mystox.framework.entity;
  * update record:
  */
 public class ServerMsg {
-
 
 
     private String myid;
@@ -20,12 +21,13 @@ public class ServerMsg {
     private String serviceUri;
     private String title;
     private String groupCode; //对应云服务编码
-//    private String serverMark; //服务端标识 对应云管的serviceVersion 默认为*
-
+    //    private String serverMark; //服务端标识 对应云管的serviceVersion 默认为*
+    private Map<String, Object> extension;
     private Long sequence;
+
     public ServerMsg(String host, int port, String serverName, String serverVersion, String routeMark,
-                     String pageRoute, String serviceUri, String title,String groupCode,String myid) {
-        this.myid=myid;
+                     String pageRoute, String serviceUri, String title, String groupCode, String myid) {
+        this.myid = myid;
         this.host = host;
         this.port = port;
         this.serverName = serverName;
@@ -35,13 +37,13 @@ public class ServerMsg {
         this.serviceUri = serviceUri;
         this.title = title;
         this.groupCode = groupCode;
-//        this.serverMark = serverMark;
+        //        this.serverMark = serverMark;
     }
 
 
     public ServerMsg(String host, int port, String serverName, String serverVersion, String routeMark,
-                     String pageRoute, String serviceUri, String title,String groupCode) {
-        this.myid=myid;
+                     String pageRoute, String serviceUri, String title, String groupCode) {
+        this.myid = myid;
         this.host = host;
         this.port = port;
         this.serverName = serverName;
@@ -51,12 +53,19 @@ public class ServerMsg {
         this.serviceUri = serviceUri;
         this.title = title;
         this.groupCode = groupCode;
-//        this.serverMark = serverMark;
+        //        this.serverMark = serverMark;
     }
 
     public ServerMsg() {
     }
 
+    public Map<String, Object> getExtension() {
+        return extension;
+    }
+
+    public void setExtension(Map<String, Object> extension) {
+        this.extension = extension;
+    }
 
     public Long getSequence() {
         return sequence;
@@ -66,8 +75,13 @@ public class ServerMsg {
         this.sequence = sequence;
     }
 
-    public String getMyid() { return myid; }
-    public void setMyid(String myid) { this.myid = myid; }
+    public String getMyid() {
+        return myid;
+    }
+
+    public void setMyid(String myid) {
+        this.myid = myid;
+    }
 
 
     public String getGroupCode() {
