dbbb8e95ec68afa62ef4f49977c0f6758f477fdf
mystox <mystox@163.com>
2021-06-30 15:44:51 +0800

    [add] 添加消息框架参数可以选择使用应用实体的demo

---
+++ yyds-server-project/yyds-opera-api/src/main/java/tech/mystox/framework/api/test/entity/ReturnEntity.java
@@ -0,0 +1,28 @@
+package tech.mystox.framework.api.test.entity;
+
+/**
+ * Created by mystox on 2021/6/28, 15:28.
+ * company:
+ * description:
+ * update record:
+ */
+public class ReturnEntity {
+    private String result;
+    private Integer code;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+}
