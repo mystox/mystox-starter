c721665198d9f9da8e4387ddc56b180ea2af137c
mystox <mystox@163.com>
2021-08-30 13:51:57 +0800

    [modify] 优雅关闭的一些优化修改

--- yyds-common/src/main/java/tech/mystox/framework/ApplicationCloseEventListener.java
+++ yyds-common/src/main/java/tech/mystox/framework/ApplicationCloseEventListener.java
@@ -2,21 +2,15 @@ package tech.mystox.framework;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 import tech.mystox.framework.config.IaConf;
 import tech.mystox.framework.core.IaContext;
-import tech.mystox.framework.core.IaRegister;
-import tech.mystox.framework.entity.RegisterSub;
 import tech.mystox.framework.entity.ServerStatus;
 import tech.mystox.framework.entity.TopicPrefix;
-import tech.mystox.framework.scheduler.MsgScheduler;
-import tech.mystox.framework.scheduler.RegScheduler;
 
 import static tech.mystox.framework.common.util.MqttUtils.*;
 
@@ -58,8 +52,6 @@ public class ApplicationCloseEventListener implements ApplicationListener<Contex
             }
         }
         while (mqttExecutor.getActiveCount() > 0 && mqttSenderAckExecutor.getActiveCount() > 0);
-        iaContext.getIaENV().getRegScheduler().unregister();
-        iaContext.getIaENV().getMsgScheduler().unregister();
         iaContext.getIaRegister().unregister();
         iaContext.getIaENV().setServerStatus(ServerStatus.OFFLINE);
     }
