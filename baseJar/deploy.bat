b4ebc94ce10496144f901dece71e13ebeffdf4c1
mystox <mystox@163.com>
2021-09-24 15:24:19 +0800

    [modify] 安装脚本的版本号控制处理

--- baseJar/deploy.bat
+++ baseJar/deploy.bat
@@ -2,18 +2,19 @@ title install-common
 set BASE_DIR=%~dp0
 set DEPLOY_URL=http://192.168.0.234:8881/nexus/repository/maven-releases/
 set REPOSITORY_Id=releases
+set VERSION=2.1.7
 
-call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-common -Dversion=2.1.7 -Dpackaging=jar -Dfile=%BASE_DIR%/yyds-common-2.1.7.jar
-call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-balancer -Dversion=2.1.7 -Dpackaging=jar -Dfile=%BASE_DIR%/yyds-balancer-2.1.7.jar
-call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-register -Dversion=2.1.7 -Dpackaging=jar -Dfile=%BASE_DIR%/yyds-register-2.1.7.jar
-call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-mqtt-starter -Dversion=2.1.7 -Dpackaging=jar -Dfile=%BASE_DIR%/yyds-mqtt-starter-2.1.7.jar
+call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-common -Dversion=%VERSION% -Dpackaging=jar -Dfile=%BASE_DIR%/yyds-common-%VERSION%.jar
+call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-balancer -Dversion=%VERSION% -Dpackaging=jar -Dfile=%BASE_DIR%/yyds-balancer-%VERSION%.jar
+call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-register -Dversion=%VERSION% -Dpackaging=jar -Dfile=%BASE_DIR%/yyds-register-%VERSION%.jar
+call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-mqtt-starter -Dversion=%VERSION% -Dpackaging=jar -Dfile=%BASE_DIR%/yyds-mqtt-starter-%VERSION%.jar
 
 
-call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-iarpc-starter -Dversion=2.1.7 -Dpackaging=pom -Dfile=%BASE_DIR%/yyds-iarpc-starter-2.1.7.xml
-call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-common -Dversion=2.1.7 -Dpackaging=pom -Dfile=%BASE_DIR%/yyds-common-2.1.7.xml
-call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-balancer -Dversion=2.1.7 -Dpackaging=pom -Dfile=%BASE_DIR%/yyds-balancer-2.1.7.xml
-call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-register -Dversion=2.1.7 -Dpackaging=pom -Dfile=%BASE_DIR%/yyds-register-2.1.7.xml
-call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-mqtt-starter -Dversion=2.1.7 -Dpackaging=pom -Dfile=%BASE_DIR%/yyds-mqtt-starter-2.1.7.xml
+call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-iarpc-starter -Dversion=%VERSION% -Dpackaging=pom -Dfile=%BASE_DIR%/yyds-iarpc-starter-%VERSION%.xml
+call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-common -Dversion=%VERSION% -Dpackaging=pom -Dfile=%BASE_DIR%/yyds-common-%VERSION%.xml
+call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-balancer -Dversion=%VERSION% -Dpackaging=pom -Dfile=%BASE_DIR%/yyds-balancer-%VERSION%.xml
+call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-register -Dversion=%VERSION% -Dpackaging=pom -Dfile=%BASE_DIR%/yyds-register-%VERSION%.xml
+call mvn deploy:deploy-file -DrepositoryId=%REPOSITORY_Id% -Durl=%DEPLOY_URL% -DgeneratePom=true -DgroupId=tech.mystox.framework -DartifactId=yyds-mqtt-starter -Dversion=%VERSION% -Dpackaging=pom -Dfile=%BASE_DIR%/yyds-mqtt-starter-%VERSION%.xml
 
 
 
