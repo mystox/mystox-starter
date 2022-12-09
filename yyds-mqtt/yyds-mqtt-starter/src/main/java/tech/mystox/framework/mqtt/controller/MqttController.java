415c2bf831dba3eaa684cc80ba49ae9e107fec84
mystox <mystox@163.com>
2022-12-09 13:33:05 +0800

    [modify] mqtt调试controller的优化

--- yyds-mqtt/yyds-mqtt-starter/src/main/java/tech/mystox/framework/mqtt/controller/MqttController.java
+++ yyds-mqtt/yyds-mqtt-starter/src/main/java/tech/mystox/framework/mqtt/controller/MqttController.java
@@ -1,14 +1,12 @@
 package tech.mystox.framework.mqtt.controller;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.web.bind.annotation.*;
 import tech.mystox.framework.common.util.SpringContextUtil;
 import tech.mystox.framework.core.IaContext;
 import tech.mystox.framework.entity.JsonResult;
@@ -33,7 +31,8 @@ import java.util.Map;
  * update record:
  */
 @RestController
-@RequestMapping("/mqtt")
+@ConditionalOnExpression("${mqtt.controller.enable:false}")
+@RequestMapping("/register")
 public class MqttController {
 
     private static final Logger logger = LoggerFactory.getLogger(MqttController.class);
@@ -51,78 +50,71 @@ public class MqttController {
 
     /**
      * 注册订阅表
-     *
-     * @return
      */
-    @RequestMapping("/registerSub")
-    public JsonResult registerSub(@RequestBody JSONObject subJson) {
-        logger.info("register sub msg" + subJson);
+    @PostMapping ("/registerSub")
+    public JsonResult<String> registerSub(@RequestBody JSONObject subJson) {
+        logger.info("Register sub msg" + subJson);
         return mqttRestService.registerSub(subJson);
     }
 
     /**
      * 注册请求表 无效
      *
-     * @return
      */
-    @RequestMapping("/registerPub")
-    public JsonResult registerPub(@RequestBody String body) {
-        logger.info("register pub msg" + body);
+    @PostMapping("/registerPub")
+    public JsonResult<?> registerPub(@RequestBody String body) {
+        logger.info("Register pub msg" + body);
         JSONObject subJson = JSONObject.parseObject(body);
-        String serverCode = subJson.getString("serverCode");
-        logger.info("往注册中心注册请求实体,跟随注册模块的实现。。。");
+//        String serverCode = subJson.getString("serverCode");
+//        logger.info("往注册中心注册请求实体,跟随注册模块的实现。。。");
         //todo
-        return new JsonResult();
+        return new JsonResult<>();
     }
 
     /**
      * 更新订阅表
      *
-     * @return
      */
-    @RequestMapping("/updateSub")
-    public JsonResult updateSub(@RequestBody JSONObject body) {
+    @PostMapping("/updateSub")
+    public JsonResult<?> updateSub(@RequestBody JSONObject body) {
         return registerSub(body);
     }
 
     /**
      * 更新请求表
      *
-     * @return
      */
-    @RequestMapping("/updatePub")
-    public JsonResult updatePub() {
+    @PostMapping("/updatePub")
+    public JsonResult<?> updatePub() {
         //todo
-        return new JsonResult();
+        return new JsonResult<>();
     }
 
     /**
      * 删除订阅表
      *
-     * @return
      */
-    @RequestMapping("/deleteSub")
-    public JsonResult deleteSub(@RequestBody JSONObject body) {
-        logger.info("delete body[{}] ...", body);
+    @PostMapping("/deleteSub")
+    public JsonResult<String> deleteSub(@RequestBody JSONObject body) {
+        logger.info("Delete body[{}] ...", body);
         return mqttRestService.deleteSub(body);
     }
 
     /**
      * 删除请求表
      *
-     * @return
      */
-    @RequestMapping("/deletePub")
-    public JsonResult deletePub(@RequestParam String topic) {
+    @PostMapping("/deletePub")
+    public JsonResult<?> deletePub(@RequestParam String topic) {
         //todo
-        logger.info("delete topic[{}] ...", topic);
+        logger.info("Delete topic[{}] ...", topic);
         //todo
         logger.info("从注册中心订阅表移除topic...注册模块实现...");
-        return new JsonResult();
+        return new JsonResult<>();
     }
 
-    @RequestMapping("/getReceiverPackageCallBack")
-    public JsonResult getReceiverPackageCallBack(@RequestParam(required = false) String msgId) {
+    @PostMapping("/getReceiverPackageCallBack")
+    public JsonResult<?> getReceiverPackageCallBack(@RequestParam(required = false) String msgId) {
         MqttReceiver bean = SpringContextUtil.getApplicationContext().getBean(MqttReceiver.class);
         Map<String, CallSubpackageMsg<MqttMsg>> callbacks = bean.getCALLBACKS();
         if (StringUtils.isNotBlank(msgId)) {
@@ -135,13 +127,13 @@ public class MqttController {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            return new JsonResult(call);
+            return new JsonResult<>(call);
         }
-        return new JsonResult(callbacks);
+        return new JsonResult<>(callbacks);
     }
 
-    @RequestMapping("/getSenderSyncCallBack")
-    public JsonResult getSenderSyncCallBack(@RequestParam(required = false) String msgId) {
+    @PostMapping("/getSenderSyncCallBack")
+    public JsonResult<?> getSenderSyncCallBack(@RequestParam(required = false) String msgId) {
         ChannelSenderImpl mqttSender = (ChannelSenderImpl) this.iaContext.getIaENV().getMsgScheduler();
         Map<String, CallSubpackageMsg<MsgRsp>> callbacks = mqttSender.getCALLBACKS();
         if (StringUtils.isNotBlank(msgId)) {
@@ -154,13 +146,13 @@ public class MqttController {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            return new JsonResult(call);
+            return new JsonResult<>(call);
         }
-        return new JsonResult(callbacks.size());
+        return new JsonResult<>(callbacks.size());
     }
 
-    @RequestMapping("/updateOperaRoute")
-    public JsonResult updateOperaRoute(@RequestBody JSONObject body) {
+    @PostMapping("/updateOperaRoute")
+    public JsonResult<?> updateOperaRoute(@RequestBody JSONObject body) {
         String operaCode = body.getString("operaCode");
         JSONArray subGroupServerArr = body.getJSONArray("subGroupServerArr");
         List<String> subGroupServerList = subGroupServerArr.toJavaList(String.class);
@@ -170,12 +162,12 @@ public class MqttController {
             //修改完成需要广播
             operaRouteService.broadcastOperaRoute(operaCode, subGroupServerList);
         } catch (InterruptedException | IOException e) {
-            logger.error("update opera route error: [{}]", e.toString());
+            logger.error("update opera route error:", e);
             if (logger.isDebugEnabled())
                 e.printStackTrace();
-            return new JsonResult("update opera route error: [{}]" + e.toString(), false);
+            return new JsonResult<>("update opera route error: " + e, false);
         }
-        return new JsonResult();
+        return new JsonResult<>();
 
     }
 
