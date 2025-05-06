5ef14b40d413e610d988aad42961a23d4ff9e370
mystox <mystox@163.com>
2025-05-06 11:34:38 +0800

    [modify] optimize logger

--- yyds-common/src/main/java/tech/mystox/framework/monitor/HealthController.java
+++ yyds-common/src/main/java/tech/mystox/framework/monitor/HealthController.java
@@ -1,7 +1,5 @@
 package tech.mystox.framework.monitor;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +18,7 @@ import tech.mystox.framework.entity.ServerStatus;
 @RequestMapping("/health")
 public class HealthController {
     final IaContext iaContext;
-    Logger logger = LoggerFactory.getLogger(HealthController.class);
+    //Logger logger = LoggerFactory.getLogger(HealthController.class);
 
     public HealthController(IaContext iaContext) {
         this.iaContext = iaContext;
